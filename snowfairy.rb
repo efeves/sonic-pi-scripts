9bd00c0f85c30b607e1855b69b7ebb96935c7a78
efeves <immortalis.sicarius@gmail.com>
2018-02-02 15:06:54 -0800

    Add files via upload

---
+++ snowfairy.rb
@@ -0,0 +1,366 @@
+#song title: Snow Fairy (Fairy Tail opening 1)
+#sheet music link https://musescore.com/user/6357021/scores/1525556
+# song bpm https://songbpm.com/snow-fairy?q=snow%20fairy
+
+use_bpm 106
+use_synth :chiplead
+
+
+play :c5
+sleep 1
+play :b4
+sleep 0.75
+play :c5
+sleep 0.75
+play :g
+sleep 0.5
+play :b4
+sleep 0.75
+play :c5
+sleep 0.75
+play :g
+sleep 0.5
+play :b4
+sleep 0.75
+play :c5
+sleep 0.75
+play :c5
+sleep 0.25
+play :e5
+sleep 0.5
+play :d5
+sleep 0.25
+play :f5
+sleep 0.75
+play :e5
+sleep 0.5
+play :c5, release: 1.5
+sleep 2
+play :d5
+sleep 0.5
+play :g
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 0.5
+play :b4
+sleep 0.5
+play :b4
+sleep 0.5
+play :c5
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 1
+play :b4
+sleep 0.75
+play :d5
+sleep 0.25
+play :c5, release: 1.5
+sleep 2
+play :d5
+sleep 0.5
+play :d5
+sleep 0.25
+play :e5
+sleep 0.25
+play :f5
+sleep 0.25
+play :e5
+sleep 0.25
+play :d5
+sleep 0.25
+play :c5
+sleep 0.25
+play :b4
+sleep 0.25
+play :c5
+sleep 0.25
+play :g5
+sleep 0.25
+play :e5
+sleep 0.75
+play :a5
+sleep 0.25
+play :g5, release: 1.5
+sleep 2
+play :f5
+sleep 0.25
+play :e5
+sleep 0.25
+play :f5
+sleep 0.25
+play :g5
+sleep 0.25
+play :c6
+sleep 0.25
+play :b5
+sleep 0.25
+play :c6, release: 1.5
+sleep 2.5
+
+live_loop :base_drum do
+  31.times do
+    sample :drum_heavy_kick
+    sleep 0.5
+    sample :drum_bass_soft
+    sleep 0.5
+  end
+  sleep 20000
+end
+
+play :e5
+sleep 1
+play :e5
+sleep 1.5
+play :d5
+sleep 0.25
+play :e5
+sleep 0.25
+play :f5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 0.5
+play :b
+sleep 0.5
+play :c5
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 1.5
+
+play :e5
+sleep 1
+play :e5
+sleep 1.5
+play :c5
+sleep 0.5
+play :b4
+sleep 0.5
+play :c5
+sleep 0.5
+play :d5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 0.5
+play :b4
+sleep 0.25
+play :c5
+sleep 0.25
+play :g
+sleep 1.5
+
+play :e5
+sleep 1
+play :e5
+sleep 1.5
+play :d5
+sleep 0.25
+play :e5
+sleep 0.25
+play :f5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 0.5
+play :b
+sleep 0.5
+play :b
+sleep 0.25
+play :c5
+sleep 0.25
+play :d5
+sleep 0.5
+play :c5
+sleep 1.5
+
+play :e5
+sleep 1
+play :e5, release: 1.5
+sleep 3
+
+play :e5
+sleep 1
+play :e5, release: 1.5
+sleep 2
+
+sample :vinyl_rewind, amp: 0.5
+sleep 4.5
+
+play :g4, release: 1.5
+sleep 2.25
+play :g4
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 0.5
+play :b4
+sleep 0.5
+play :c5
+sleep 0.5
+play :d5
+sleep 0.5
+play :e5
+sleep 0.5
+play :f5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 2.5
+
+play :g4
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 0.5
+play :b4
+sleep 0.5
+play :c5
+sleep 0.5
+play :d5
+sleep 0.5
+play :e5
+sleep 0.5
+play :f5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :e5
+sleep 2.5
+
+play :g4
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 0.5
+play :b4
+sleep 0.5
+play :c5
+sleep 0.5
+play :d5
+sleep 0.5
+play :e5
+sleep 0.5
+play :f5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :e5
+sleep 2.5
+
+play :e5
+sleep 1
+play :e5, release: 1.5
+sleep 3
+
+play :e5
+sleep 1
+play :e5
+sleep 1.5
+play :d5
+sleep 0.25
+play :e5
+sleep 0.25
+play :f5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 0.5
+play :b
+sleep 0.5
+play :c5
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 1.5
+
+play :e5
+sleep 1
+play :e5
+sleep 1.5
+play :c5
+sleep 0.5
+play :b4
+sleep 0.5
+play :c5
+sleep 0.5
+play :d5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 0.5
+play :b4
+sleep 0.25
+play :c5
+sleep 0.25
+play :g
+sleep 1.5
+
+play :e5
+sleep 1
+play :e5
+sleep 1.5
+play :d5
+sleep 0.25
+play :e5
+sleep 0.25
+play :f5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 0.5
+play :b
+sleep 0.5
+play :b
+sleep 0.25
+play :c5
+sleep 0.25
+play :d5
+sleep 0.5
+play :c5
+sleep 1.5
+
+play :e5
+sleep 1
+play :e5, release: 1.5
+sleep 3
+
+play :e5
+sleep 1
+play :e5, release: 1.5
+sleep 2
+
