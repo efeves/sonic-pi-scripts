34531c0127d258bcd2161ea29cca923105038a7d
efeves <immortalis.sicarius@gmail.com>
2018-02-08 13:35:42 -0800

    Update codechallenge2.rb

--- codechallenge2.rb
+++ codechallenge2.rb
@@ -1,9 +1,9 @@
 use_bpm 60
 use_synth :piano
 
-var = 21
-88.times do
-  play var
+var = 21 #sets a variable "var" as the value 21
+88.times do #play this code 88 times
+  play var #play the aforementioned variable
   sleep 0.25
-  var=var+1
+  var=var+1 #increase the value of the variable by one and make this the new variable.
 end
